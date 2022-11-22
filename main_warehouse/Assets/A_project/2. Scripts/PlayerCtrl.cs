5b25a5618ec5bdb36144cd83dcbf624ac46d06d4
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2022-11-22 15:12:14 +0900

    Revert "pull request main to Main warehouse 2"

--- main_warehouse/Assets/A_project/2. Scripts/PlayerCtrl.cs
+++ main_warehouse/Assets/A_project/2. Scripts/PlayerCtrl.cs
@@ -7,85 +7,6 @@ using Unity.XR.PXR;
     public class PlayerCtrl : MonoBehaviour
     {
         public float speed = 12.0f;
-    public float ray_distance = 1.0f;
-        private InputDevice currentController;
-        private PXR_Input.Controller controller;
-        private Vector2 axis2D;
-
-        private Transform tr;
-        private Transform camtr;
-        private Camera cam;
-        private float dirX;
-        private float dirZ;
-        private bool pax;
-    private Vector3 ScreenCenter;
-    Ray ray;
-    RaycastHit hit;
-    private void Start()
-        {
-            currentController = InputDevices.GetDeviceAtXRNode(controller == PXR_Input.Controller.LeftController ? XRNode.LeftHand : XRNode.RightHand);
-            // À½..
-            tr = GetComponent<Transform>();
-            camtr = Camera.main.GetComponent<Transform>();
-        ScreenCenter = new Vector3(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2);
-        StartCoroutine("Blocking");
-        }
-        private void Update()
-        {
-            //test();
-            MovePrimary2DAxis();
-        //StartCoroutine("Blocking");
-    }
-        private void test()
-        {
-            if (currentController.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out pax) && pax)
-            {
-                Debug.Log("primary2DClick");
-                currentController.TryGetFeatureValue(CommonUsages.primary2DAxis, out axis2D);
-                Debug.Log("joystick dirx: " + axis2D.x + " / diry: ");
-            }
-        }
-        private void MovePrimary2DAxis()
-        {
-        ray = Camera.main.ScreenPointToRay(ScreenCenter);
-        int mask = (1 << 7);
-        dirX = 0;
-            if (currentController.TryGetFeatureValue(CommonUsages.primary2DAxis, out axis2D))
-            {
-                    if (axis2D.y < 0|| Physics.Raycast(ray, out hit, ray_distance, mask))
-                    {
-                        return;
-                    }
-                var inputVector = new Vector3(dirX, 0, axis2D.y);
-                //var inputDir = transform.TransformDirection(inputVector);
-                var lookDir = new Vector3(0, camtr.eulerAngles.y, 0);
-                var newDir = Quaternion.Euler(lookDir) * inputVector;
-                //transform.Translate(moveDir * Time.deltaTime);
-                transform.Translate(newDir * Time.deltaTime * speed);
-            }
-        }
-   /* IEnumerator Blocking()
-    {
-        ray = Camera.main.ScreenPointToRay(ScreenCenter);
-        int mask = (1 << 7);
-        while (Physics.Raycast(ray, out hit, ray_distance, mask))
-        {
-            Debug.Log(hit.collider.gameObject.name);
-            yield return null;
-        }
-
-    }*/
-}
-/*
- using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.XR;
-using Unity.XR.PXR;
-
-    public class PlayerCtrl : MonoBehaviour
-    {
-        public float speed = 12.0f;
         public float shakeY = 2.0f;
         public float shakeTime =1.0f;
         private InputDevice currentController;
@@ -155,4 +76,4 @@ using Unity.XR.PXR;
                 yield return null;
             }
         }
-}*/
+}

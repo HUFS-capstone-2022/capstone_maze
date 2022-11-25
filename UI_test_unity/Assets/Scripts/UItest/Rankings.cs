d78c3b24f6f115b608fe4b2363023aac9e6a9324
username <jjyoyo83@gmail.com>
2022-11-25 16:19:40 +0900

    finish UI

---
+++ UI_test_unity/Assets/Scripts/UItest/Rankings.cs
@@ -0,0 +1,17 @@
+// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// namespace Rankings
+// {
+//     public class Boards
+//     {
+//         public class True_end
+//         {
+//             public int userId;
+//             public string clear_type;
+//             public 
+//         }
+//     }
+// }
